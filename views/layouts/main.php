585ad2f307b49b4087854f32940e9c8e2a12583d
Zhukarev <jukarev@gmail.com>
2017-11-30 20:26:21 +0200

    info

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -39,6 +39,7 @@ AppAsset::register($this);
         'options' => ['class' => 'navbar-nav navbar-right'],
         'items' => [
             ['label' => 'Home', 'url' => ['/site/index']],
+            ['label' => 'Users', 'url' => ['/users/index']],
             ['label' => 'Map', 'url' => ['/site/map']],
             ['label' => 'Contact', 'url' => ['/site/contact']],
             Yii::$app->user->isGuest ? (
